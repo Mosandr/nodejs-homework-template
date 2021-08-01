ea0f44c59b5a41754977f543e999127ab4ba0907
Andriy Mosol <mosandr5@gmail.com>
2021-08-01 22:56:41 +0300

    Add saving crop avatar to public folder

--- controllers/users/updateAvatar.js
+++ controllers/users/updateAvatar.js
@@ -3,27 +3,50 @@ const { HttpCode } = require('../../helpers/constants')
 const fs = require('fs/promises')
 const path = require('path')
 require('dotenv').config()
+const Jimp = require('jimp')
+const cloudinary = require('cloudinary').v2
+
+const { API_KEY, API_SECRET, CLOUD_NAME } = process.env
+
+cloudinary.config({
+  cloud_name: CLOUD_NAME,
+  api_key: API_KEY,
+  api_secret: API_SECRET,
+  secure: true,
+})
 
 const updateAvatar = async (req, res, next) => {
   try {
     const { _id: userId } = req.user
     const { path: filepath, originalname } = req.file
 
-    console.log(req.file)
-
     if (!req.file) {
       return next({
         status: HttpCode.BAD_REQUEST,
         message: 'Missing field avatar',
-        data: 'Bad Request'
+        data: 'Bad Request',
       })
     }
 
     const destination = path.normalize(
-      path.join(__dirname, '\\..\\..', process.env.AVATARS_DIR, originalname)
+      path.join(
+        __dirname,
+        '\\..\\..',
+        process.env.AVATARS_DIR,
+        userId.toString() + '_' + Date.now() + '_' + originalname,
+      ),
     )
 
     if (req.file) {
+      const img = await Jimp.read(filepath)
+      await img
+        .autocrop()
+        .cover(
+          250,
+          250,
+          Jimp.HORIZONTAL_ALIGN_CENTER || Jimp.VERTICAL_ALIGN_MIDDLE,
+        )
+        .writeAsync(filepath)
       await fs.rename(filepath, destination)
     }
 
@@ -37,15 +60,15 @@ const updateAvatar = async (req, res, next) => {
         data: {
           user: {
             email: user.email,
-            avatarURL: user.avatarURL
-          }
-        }
+            avatarURL: user.avatarURL,
+          },
+        },
       })
     } else {
       return next({
         status: HttpCode.NOT_FOUND,
         message: 'Not found user to update',
-        data: 'Not Found'
+        data: 'Not Found',
       })
     }
   } catch (error) {
